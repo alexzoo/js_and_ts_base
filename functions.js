2e353e87f9107afdb097aae251f17728a75559e0
azubov <azubov@provectus.com>
2022-06-23 14:34:27 +0300

    add exercises

--- functions.js
+++ functions.js
@@ -35,3 +35,37 @@ function lessTen(num) {
 }
 
 console.log(lessTen(120));
+
+//⋕js.Pm.UF.Ex.2
+function getDigitsSum(num) {
+    let digits = String(num).split('')
+    let sum = 0
+    for (let el of digits) {
+        sum += +el
+    }
+    return sum
+}
+
+console.log(getDigitsSum(555));
+
+
+//⋕js.Pm.UF.Ex.4
+function reverseStr(str) {
+    return str.split('').reverse().join('')
+}
+
+console.log(reverseStr('hello world'));
+
+//⋕js.Pm.UF.Ex.5
+function delElem(arr, num) {
+    let i = 0;
+    while (i < arr.length) {
+      if (arr[i] === num) {
+        arr.splice(i, 1);
+      } else {
+        ++i;
+      }
+    }
+    return arr;
+}
+console.log(delElem([3, 5, 2, 5], 5));
